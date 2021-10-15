eeea1ddc7a2c8a3bbd87e5b32b375b86fa8b4f75
ayonymus <ayonymus@gmail.com>
2021-10-15 17:58:21 +0200

    Fix unit test

--- test/features/workbranch_test.py
+++ test/features/workbranch_test.py
@@ -14,10 +14,11 @@ class TestWorkBranchMethods(unittest.TestCase):
         self.storage = Mock()
         self.git = Mock()
         self.checkout = Mock()
-        self.wb = Workbranch(self.storage, self.checkout, self.git)
+        self.wb = Workbranch(self.git, self.storage, self.checkout)
 
     def test_current_work_branch_should_be_None(self):
         self.storage.load_work_branches.return_value = []
+        print(self.wb.get_work_branch())
         self.assertIsNone(self.wb.get_work_branch())
 
     def test_current_work_branch_is_last_in_history(self):
